07dd59a9e87a6b429369794e8febb6048b0f8092
Tia Iskanius <isktia@helsinki.fi>
2023-04-04 17:24:53 +0300

    tests added to routes_test.py

--- ticket_to_ride_app/src/tests/routes_test.py
+++ ticket_to_ride_app/src/tests/routes_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from routes import Routes
 
-class TestTrainGenerator(unittest.TestCase):
+class TestRoutes(unittest.TestCase):
     def setUp(self):
         self.route = Routes()
 
@@ -10,3 +10,9 @@ class TestTrainGenerator(unittest.TestCase):
     def test_get_route_returns_false_if_doesnt_exist(self):
         self.assertEqual(self.route.get_route(1,16), False)
     
+    def test_get_route_returns_right_things(self):
+        self.assertEqual(self.route.get_route(1,2), [1, 1, 0])
+    
+    def test_buy_routes_works(self):
+        self.route.buy_route(1,2,1)
+        self.assertEqual(self.route.get_route(1,2)[2], 1)
